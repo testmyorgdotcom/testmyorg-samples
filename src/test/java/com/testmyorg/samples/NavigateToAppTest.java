c7df12a52795ba6fbd94f34b0f8e318122922fe7
baksale <java.rabota@gmail.com>
2021-05-09 21:53:40 +0300

    example of navigation to app

--- src/test/java/com/testmyorg/samples/NavigateToAppTest.java
+++ src/test/java/com/testmyorg/samples/NavigateToAppTest.java
@@ -1,18 +1,27 @@
-package com.testmyorg.samples.navigation;
+package com.testmyorg.samples;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
 import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.testmy.screenplay.fact.PersonaBehaviour;
 import org.testmy.screenplay.factory.Login;
 import org.testmy.screenplay.factory.ability.Authenticate;
+import org.testmy.screenplay.factory.ability.Call;
+import org.testmy.screenplay.factory.ability.NavigateTo;
+import org.testmy.screenplay.question.ui.AppName;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
 
+@RunWith(SerenityRunner.class)
 public class NavigateToAppTest {
     @Managed
     WebDriver browser;
@@ -24,12 +33,16 @@ public class NavigateToAppTest {
         salesManager.has(PersonaBehaviour.of("Sales Manager"));
         salesManager
                 .can(Authenticate.withCredentials())
+                .can(Call.partnerApi())
                 .can(BrowseTheWeb.with(browser))
-                .wasAbleTo(Login.withSessionIdAsCookies());
+                .wasAbleTo(Login.withSessionIdInFrontDoorUrl());
     }
 
     @Test
     public void navigateToSalesApp() {
-        when(salesManager).attemptsTo(NavigateToApp.called("Sales"));
+        final String appName = "Sales";
+        when(salesManager).attemptsTo(NavigateTo.appCalled(appName));
+
+        then(salesManager).should(seeThat(AppName.displayed(), is(appName)));
     }
 }
