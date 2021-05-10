f05b91f5bbeb865f97e1d206c025ac909ca27841
baksale <java.rabota@gmail.com>
2021-05-10 23:11:15 +0300

    global actions initial sample plus refactoring of the existing tests to avoid code duplications

---
+++ src/test/java/com/testmyorg/samples/GlobalActionTest.java
@@ -0,0 +1,32 @@
+package com.testmyorg.samples;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.givenThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static org.testmy.data.matchers.Matchers.contact;
+import static org.testmy.data.matchers.Matchers.hasField;
+import static org.testmy.data.matchers.Matchers.hasId;
+import static org.testmy.data.matchers.Matchers.ofShape;
+
+import org.junit.Test;
+import org.testmy.data.matchers.HasFields;
+import org.testmy.screenplay.act.task.CreateRecord;
+import org.testmy.screenplay.factory.Login;
+import org.testmy.screenplay.question.QueryData;
+
+public class GlobalActionTest extends DemoTest {
+    @Test
+    public void createContactViaGlobalActions () {
+        final HasFields ofShape = ofShape(
+            contact(),
+            hasField("LastName", "Test User Last Name")
+        );
+
+        givenThat(demoActor).wasAbleTo(Login.withSessionIdInFrontDoorUrl());
+
+        when(demoActor).attemptsTo(CreateRecord.viaGlobalAction(ofShape));
+
+        then(demoActor).should(seeThat(QueryData.similarTo(ofShape), hasId()));
+    }
+}
